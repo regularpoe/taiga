3ef1d95aa4c570bba26d7fdcf3138c6604376dcb
regularpoe <vedranvinko@protonmail.com>
2024-09-02 20:53:53 +0200

    Feed fake data and insert into DB

--- Program.fs
+++ Program.fs
@@ -23,6 +23,17 @@ let createTableSql (tableName: String) (schemaFilePath: String) =
 
     sprintf "CREATE TABLE IF NOT EXISTS %s (%s);" tableName columns
 
+let createInsertSql (tableName: string) (data: JObject) =
+    let columns = data.Properties() |> Seq.map (fun prop -> prop.Name) |> String.concat ", "
+    let values = data.Properties() |> Seq.map (fun prop -> sprintf "@%s" prop.Name) |> String.concat ", "
+    let sql = sprintf "INSERT INTO %s (%s) VALUES (%s);" tableName columns values
+    sql
+
+let jsonToDict (data: JObject) =
+    data.Properties()
+    |> Seq.map (fun prop -> prop.Name, prop.Value.ToObject(typeof<obj>))
+    |> dict
+
 [<EntryPoint>]
 let main argv =
     let schemaPath =
@@ -35,6 +46,11 @@ let main argv =
         | Some index when index + 1 < argv.Length -> argv.[index + 1]
         | _ -> failwith "Table name not provdided"
 
+    let dataPath =
+        match Array.tryFindIndex (fun x -> x = "-d") argv with
+        | Some index when index + 1 < argv.Length -> Some argv.[index + 1]
+        | _ -> None
+
     let connectionString = "Data Source=database.db"
 
     let createTableCommand = createTableSql tableName schemaPath
@@ -45,6 +61,17 @@ let main argv =
     printfn "Executing SQL: %s" createTableCommand
     connection.Execute(createTableCommand) |> ignore
 
-    printfn "Table '%s' created successfully in SQLite database." tableName
+    match dataPath with
+    | Some path ->
+        let jsonData = File.ReadAllText(path) |> JArray.Parse
+        for item in jsonData do
+            let insertCommand = createInsertSql tableName (item :?> JObject)
+            let parameters = jsonToDict (item :?> JObject)
+            connection.Execute(insertCommand, parameters) |> ignore
+            printfn "Inserted data: %s" (item.ToString())
+    | None -> 
+        printfn "No data file provided, skipping data insertion."
+
+    printfn "Table '%s' created and data inserted successfully." tableName
 
     0
